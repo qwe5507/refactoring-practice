ac8b3e41e09d916df0d8b023fb424de2663ef7a4
qwe5507 <apvmffkdls@gmail.com>
2023-10-23 00:03:11 +0900

    리팩토링 4. 함수 추출하기
    - 메소드를 한줄짜리 메소드로 추출해도 좋다.
    - 각각의 주석을 함수이름으로 추출하면 좋다.

--- src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
+++ src/main/java/me/whiteship/refactoring/_02_duplicated_code/_04_extract_function/StudyDashboard.java
@@ -12,30 +12,41 @@ public class StudyDashboard {
 
     private void printParticipants(int eventId) throws IOException {
         // Get github issue to check homework
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(eventId);
+        GHIssue issue = getGhIssue(eventId);
 
         // Get participants
-        Set<String> participants = new HashSet<>();
-        issue.getComments().forEach(c -> participants.add(c.getUserName()));
+        Set<String> participants = getUsernames(issue);
 
         // Print participants
-        participants.forEach(System.out::println);
+        print(participants);
     }
 
     private void printReviewers() throws IOException {
         // Get github issue to check reviews
-        GitHub gitHub = GitHub.connect();
-        GHRepository repository = gitHub.getRepository("whiteship/live-study");
-        GHIssue issue = repository.getIssue(30);
+        GHIssue issue = getGhIssue(30);
 
         // Get reviewers
-        Set<String> reviewers = new HashSet<>();
-        issue.getComments().forEach(c -> reviewers.add(c.getUserName()));
+        Set<String> reviewers = getUsernames(issue);
 
         // Print reviewers
-        reviewers.forEach(System.out::println);
+        print(reviewers);
+    }
+
+    private static void print(final Set<String> participants) {
+        participants.forEach(System.out::println);
+    }
+
+    private static Set<String> getUsernames(final GHIssue issue) throws IOException {
+        Set<String> usernames = new HashSet<>();
+        issue.getComments().forEach(c -> usernames.add(c.getUserName()));
+        return usernames;
+    }
+
+    private static GHIssue getGhIssue(final int eventId) throws IOException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        GHIssue issue = repository.getIssue(eventId);
+        return issue;
     }
 
     public static void main(String[] args) throws IOException {
